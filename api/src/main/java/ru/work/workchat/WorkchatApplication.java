a70f99fc6ff70c40eaec06f6dad6a598fe35c8fe
Kirill <kirill-22ru@yandex.ru>
2025-04-29 10:12:20 +0300

    создан проект spring

---
+++ api/src/main/java/ru/work/workchat/WorkchatApplication.java
@@ -0,0 +1,13 @@
+package ru.work.workchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkchatApplication.class, args);
+	}
+
+}

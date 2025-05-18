d67348ca104284423907ad4e5e9dabbee1e92ec2
kirill <kirill-22ru@yandex.ru>
2025-05-18 11:37:55 +0300

    создана ветка для деплоя в облаке

--- api/src/main/java/ru/work/workchat/configuration/WebSocketConfiguration.java
+++ api/src/main/java/ru/work/workchat/configuration/WebSocketConfiguration.java
@@ -20,7 +20,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/webs")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("https://niktopol.github.io")
                 .withSockJS();
     }
 }
--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/chat_curs",
+    assetPrefix: "/chat_curs",
     output: "export",
 };
 
